b19e385e9e98a6e5530e67e6c264f96e9fe3434a
Codjo Christian GUEGUELIGUE <krissiankg@gmail.com>
2026-02-20 20:22:28 +0100

    fix: Vercel build missing API key for Resend

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,9 +1,8 @@
 
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const body = await req.json();
     const { name, email, subject, message } = body;
 
